807f5d04f8880c839aad8c457eac038d1ce12079
Stuart Dum <stuart.dum@gmail.com>
2024-02-01 16:02:19 -0500

    Add smooth scrolling

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ document
   .addEventListener("click", function (_event) {
     document.querySelector("header").classList.toggle("mobile-nav-active");
   });
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
