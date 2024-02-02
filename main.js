abfe44a6529c40afcf3e9139ea1fbdd6af370e75
Stuart Dum <stuart.dum@gmail.com>
2024-02-02 11:10:17 -0500

    Make FAQs interactive

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 document
+  .querySelectorAll(".faq-title")
+  .forEach((title) =>
+    title.addEventListener("click", (event) =>
+      event.target.closest(".faq").classList.toggle("faq-active")
+    )
+  );
+
+document
   .getElementById("mobile-nav")
-  .addEventListener("click", function (_event) {
-    document.querySelector("header").classList.toggle("mobile-nav-active");
-  });
+  .addEventListener("click", () =>
+    document.querySelector("header").classList.toggle("mobile-nav-active")
+  );
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
 
     document.querySelector(this.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
